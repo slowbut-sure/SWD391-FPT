23f0847f8edca949d733711a6fb6f301199be0a5
slowbut-sure <thanhtruongdinh2002@gmail.com>
2023-09-24 13:57:06 +0700

    'add'

---
+++ ManagerApartmentSolution/Application/IRepository/IGeneric/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IRepository.IGeneric
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(object id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        T SingleOrDefault(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ ManagerApartmentSolution/Infrastructures/Repository/RequestLogRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructures.Repository
+{
+    public class RequestLogRepository : IRequestLogRepository
+    {
+    }
+}
