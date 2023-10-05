92c0de4b5e2ddefb0a5927e6d3a1dd43e35b2271
slowbut-sure <thanhtruongdinh2002@gmail.com>
2023-10-05 21:23:04 +0700

    update

---
+++ ManagerApartmentSolution/Service/Authentication/Implement/Authentication.cs
@@ -0,0 +1,14 @@
+﻿using ManagerApartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Authentication.Implement
+{
+    public class Authentication : IAuthentication
+    {
+
+    }
+}
