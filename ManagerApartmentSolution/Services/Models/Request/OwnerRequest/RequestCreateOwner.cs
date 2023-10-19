742f075a07bc928dbd7cb5006211efef6fb05846
slowbut-sure <thanhtruongdinh2002@gmail.com>
2023-10-19 13:45:53 +0700

    validate staff and owner

--- ManagerApartmentSolution/Services/Models/Request/OwnerRequest/RequestCreateOwner.cs
+++ ManagerApartmentSolution/Services/Models/Request/OwnerRequest/RequestCreateOwner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,21 @@ namespace Services.Models.Request.OwnerRequest
 {
     public class RequestCreateOwner
     {
+        [Required(ErrorMessage = "The code field is required.")]
         public string OwnerCode { get; set; }
+
+        [Required(ErrorMessage = "The name field is required.")]
         public string OwnerName { get; set; }
-        public string OwnerEmail { get; set; }
+
+        public string OwnerEmail { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Password field is required.")]
         public string OwnerPassword { get; set; }
+
+        [RegularExpression(@"^[0-10]{9,10}$", ErrorMessage = "The phone number must be an 9 or 10 digit integer.")]
+        [Required(ErrorMessage = "Phone field is required.")]
         public string OwnerPhone { get; set; }
-        public string OwnerAddress { get; set; }
+
+        public string OwnerAddress { get; set; } = null!;
     }
 }
--- ManagerApartmentSolution/Services/Models/Request/StaffRequest/RequestCreateStaff.cs
+++ ManagerApartmentSolution/Services/Models/Request/StaffRequest/RequestCreateStaff.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,27 @@ namespace Services.Models.Request.StaffRequest
 {
     public class RequestCreateStaff
     {
+        [Required(ErrorMessage = "The role field is required.")]
         public string StaffRole { get; set; }
 
+        [Required(ErrorMessage = "The email field is required.")]
         public string StaffEmail { get; set; }
 
+        [Required(ErrorMessage = "The name field is required.")]
         public string StaffName { get; set; }
 
+        [RegularExpression(@"^[0-10]{9,10}$", ErrorMessage = "The phone number must be an 9 or 10 digit integer.")]
+        [Required(ErrorMessage = "Phone field is required.")]
         public string StaffPhone { get; set; }
 
+        [Required(ErrorMessage = "The Password field is required.")]
         public string StaffPassword { get; set; }
 
-        public string StaffAddress { get; set; }
+        public string StaffAddress { get; set; } = null!;
 
-        public string AvatarLink { get; set; }
+        public string AvatarLink { get; set; } = null!;
 
+        [Required(ErrorMessage = "The email field is required.")]
         public string StaffCode { get; set; }
     }
 }
