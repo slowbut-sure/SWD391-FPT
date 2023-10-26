b8ca1015ec8fff6ed2dc9102792b1eda9a6cd7e7
khanghuu <huukhanggl02@gmail.com>
2023-10-26 09:19:19 +0700

    update login response

--- ManagerApartmentSolution/Services/Models/Response/TennantResponse/ResponseAccountTennant.cs
+++ ManagerApartmentSolution/Services/Models/Response/TennantResponse/ResponseAccountTennant.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums.Role;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,16 @@ namespace Services.Models.Response.TennantResponse
 {
     public class ResponseAccountTennant : AccountResponse
     {
-        public string TennantCode { get; set; }
-        public string TennantName { get; set; }
-        public string TennantEmail { get; set; }
-        public string TennantStatus { get; set; }
-        public string TennantPhone { get; set; }
-        public string TennantAddress { get; set; }
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Role { get; set; } = ROLEACCOUNT.TENANT.ToString();
+        public string? AvatarLink { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string? Status { get; set; }
     }
 }
